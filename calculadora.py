ef1e32ca50d2f22574ce7108e03aa302b4113b38
Marcella Palazzo <marcellapalazzo26@hotmail.com>
2025-04-16 18:31:10 -0300

    Adiciona loop de execução contínua e arredonda resultado para 1 casa decimal

--- calculadora.py
+++ calculadora.py
@@ -1,23 +1,35 @@
 print("Calculadora Simples")
-num1 = float(input("Digite o primeiro número: "))
-operador = input("Digite o operador (+, -, *, /): ")
-num2 = float(input("Digite o segundo número: "))
 
-if operador == "+":
-    resultado = num1 + num2
-elif operador == "-":
-    resultado = num1 - num2
-elif operador == "*":
-    resultado = num1 * num2   
-elif operador == "/":
-    if num2 != 0:
-        resultado = num1 / num2
-    else:
-        resultado = "Erro! Divisão por zero."
-elif operador == "^":
-    resultado = num1 ** num2        
-else:
-    resultado = "Operador inválido."
+while True:
+  num1 = float(input("Digite o primeiro número: "))
+  operador = input("Digite o operador (+, -, *, /, ^): ")
+  num2 = float(input("Digite o segundo número: "))
 
-print("Resultado:", resultado)                
+  if operador == "+":
+      resultado = num1 + num2
+  elif operador == "-":
+      resultado = num1 - num2
+  elif operador == "*":
+      resultado = num1 * num2   
+  elif operador == "/":
+      if num2 != 0:
+          resultado = num1 / num2
+      else:
+          resultado = "Erro! Divisão por zero."
+  elif operador == "^":
+      resultado = num1 ** num2        
+  else:
+      resultado = "Operador inválido."
+
+  if isinstance(resultado, (int, float)):
+      print("Resultado: ", round(resultado, 1))
+  else:
+      print("Resultado: ", resultado)    
+
+  continuar = input("Deseja realizar outra operação? (s/n): ").lower()
+  if continuar != "s":
+      print("Encerrando a calculadora. Até a próxima!") 
+      break   
+
+                      
         
